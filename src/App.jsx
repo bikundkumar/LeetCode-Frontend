74fb9f95593f27f741daac7b67005faa5ba6c3ba
bikundkumar <bikund2017@outlook.com>
2025-05-28 11:43:50 +0530

    create route & style the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,20 @@
 function App() {
-  const problems = [
-    {
-      title: "1. Two Sum",
-      difficulty: "Easy",
-      acceptance: "55.7%",
-    },
-    {
-      title: "2. Add Two Numbers",
-      difficulty: "Medium",
-      acceptance: "46.1%",
-    },
-    {
-      title: "4. Median of Two Sorted Arrays",
-      difficulty: "Hard",
-      acceptance: "43.6%",
-    },
-  ];
   return (
-    <div>
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
-      <button>Sign in</button>
-      <div>
-        <button>1</button>
-        <button>2</button>
-      </div>
-      <div>
-        {problems.map((problem) => (
-          <ProblemStatement
-            title={problem.title}
-            difficulty={problem.difficulty}
-            acceptance={problem.acceptance}
-          />
-        ))}
-      </div>
+    <div class="home-container">
+      <nav>
+        <ul>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/signup">Signup</a>
+          </li>
+          <li>
+            <a href="/problemset/all">Problems</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  );
-}
-
-function ProblemStatement(props) {
-  const title = props.title;
-  const difficulty = props.difficulty;
-  const acceptance = props.acceptance;
-
-  return (
-    <tr>
-      <td>{title}</td>
-      <td>{difficulty}</td>
-      <td>{acceptance}</td>
-    </tr>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import App from "./App";
 import './index.css'
-import App from './App.jsx'
+import LoginPage from "./Login/Login";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const problems = [
+  {
+    id: 1,
+    title: "200. Number of Islands",
+    difficulty: "Medium",
+    acceptance: "62.2%",
+  },
+  {
+    id: 2,
+    title: "201. Bitwise AND of Numbers Range",
+    difficulty: "Medium",
+    acceptance: "47.7",
+  },
+  {
+    id: 3,
+    title: "202. Happy Number",
+    difficulty: "Easy",
+    acceptance: "58.0%",
+  },
+  {
+    id: 4,
+    title: "203. Remove Linked List Elements",
+    difficulty: "Easy",
+    acceptance: "51.7%",
+  },
+  {
+    id: 5,
+    title: "204. Count Primes",
+    difficulty: "Medium",
+    acceptance: "34.7%",
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/problemset/all",
+    element: <ProblemSetPage problems={problems} />,
+  },
+  {
+    path: "/problems/:problem_slug",
+    element: <SingleProblemPage problems={problems} />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ReactProvider router={router} />
+  </React.StrictMode>,
 )
